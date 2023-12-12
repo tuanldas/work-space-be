50843362c1838c73ba8873dd3f41da9e3821ee43
tuanldas <tuanldas@gmail.com>
2023-12-12 14:34:34 +0700

    Khởi tạo dự án

---
+++ resources/views/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+@section('title') @lang('translation.dashboards') @endsection
+@section('content')
+
+
+
+@endsection
+@section('script')
+<script src="{{ URL::asset('build/js/app.js') }}"></script>
+@endsection
