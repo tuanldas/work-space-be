a51336c8328214d03cc9f73300431cc0e822baae
tuanldas <tuanldas@gmail.com>
2024-03-12 10:15:58 +0700

    Tạo event repeats model

--- database/factories/EventRepeatModelFactory.php
+++ database/factories/EventRepeatModelFactory.php
@@ -5,16 +5,8 @@ namespace Database\Factories;
 use App\Models\EventModel;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\EventRepeatModel>
- */
 class EventRepeatModelFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
