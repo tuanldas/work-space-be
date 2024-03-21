9024a56660b35922e08e4de023c4a5d7cfbe9340
tuanldas <tuanldas@gmail.com>
2024-03-21 14:37:22 +0700

    Tạo eloquent repository

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\Repositories\EloquentRepository;
 use App\Repositories\Interface\EventRepositoryInterface;
 use App\Repositories\EventRepository;
+use App\Repositories\Interface\RepositoryInterface;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -11,6 +13,10 @@ class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
+        $this->app->bind(
+            RepositoryInterface::class,
+            EloquentRepository::class
+        );
         $this->app->bind(
             EventRepositoryInterface::class,
             EventRepository::class
---
+++ app/Repositories/EloquentRepository.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Repositories\Interface\RepositoryInterface;
+
+class EloquentRepository implements RepositoryInterface
+{
+    protected $model;
+
+    public function __construct()
+    {
+        $this->setModel();
+    }
+
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    public function setModel(): void
+    {
+        $this->model = app()->make($this->getModel());
+    }
+
+    public function paginate($perPage = 15)
+    {
+        return $this->model->paginate($perPage);
+    }
+
+    public function save($object)
+    {
+        $object->save();
+        return $object;
+    }
+
+    public function delete($id)
+    {
+        return $this->model->destroy($id);
+    }
+
+    public function find($id, $columns = ['*'])
+    {
+        return $this->model->select($columns)->find($id);
+    }
+}
