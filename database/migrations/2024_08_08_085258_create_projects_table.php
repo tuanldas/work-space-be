b1c2109f4511739210ffaf370cc2cef1092476c1
Tuanldas <tuanldas@gmail.com>
2024-08-08 16:01:48 +0700

    Thêm các migrate

---
+++ database/migrations/2024_08_08_085258_create_projects_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->uuid()->index();
+            $table->string('name');
+            $table->text('desc')->nullable();
+            $table->string('icon');
+            $table->date('due_date')->nullable();
+            $table->tinyInteger('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
---
+++ database/migrations/2024_08_08_085311_create_columns_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('columns', function (Blueprint $table) {
+            $table->id();
+            $table->uuid()->index();
+            $table->string('title', 255);
+            $table->integer('position');
+            $table->bigInteger('project_id')->unsigned();
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('columns');
+    }
+};
